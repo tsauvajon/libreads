0097537d0889030b1455786621970465d77591e0
Thomas Sauvajon <thomas@sauvajon.tech>
2022-04-13 09:29:33 +0200

    Add an integration test

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ mod goodreads;
 mod libgen;
 mod library;
 
+use library::DownloadLinks;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    process("https://www.goodreads.com/book/show/1048424.Governing_the_Commons").await
+    let download_links =
+        process("https://www.goodreads.com/book/show/1048424.Governing_the_Commons").await?;
+    println!("IPFS.io download link: {}", download_links.ipfs_dot_io);
+
+    Ok(())
 }
 
-async fn process(goodreads_book_url: &str) -> Result<(), Error> {
+async fn process(goodreads_book_url: &str) -> Result<DownloadLinks, Error> {
     let (isbn10, isbn13) = goodreads::get_isbn(goodreads_book_url).await?;
 
     let isbn = if let Some(isbn) = isbn13 {
@@ -37,10 +42,23 @@ async fn process(goodreads_book_url: &str) -> Result<(), Error> {
         &book_metadata.extension
     );
 
-    let download_links = library::get_download_links(book_metadata.md5.as_str()).await?;
-    println!("IPFS.io download link: {}", download_links.ipfs_dot_io);
+    Ok(library::get_download_links(book_metadata.md5.as_str()).await?)
+}
 
-    Ok(())
+#[tokio::test]
+#[ignore = "This test calls live web pages and APIs, no need to run it with every file save."]
+async fn test_process() {
+    let test_url = "https://www.goodreads.com/book/show/1048424.Governing_the_Commons";
+    let got = process(test_url).await.expect("Should get download links");
+
+    assert_eq!(
+        "https://ipfs.io/ipfs/bafykbzacedqn6erurfdw45jy4xbwldyh3ihqykr2kp3sx7knm6lslzcj66m76?filename=%28Political%20Economy%20of%20Institutions%20and%20Decisions%29%20Elinor%20Ostrom%20-%20Governing%20the%20Commons_%20The%20Evolution%20of%20Institutions%20for%20Collective%20Action%20%28Political%20Economy%20of%20Institutions%20and%20Decisions%29-Cambridge.djvu",
+        got.ipfs_dot_io
+    );
+    assert_eq!(
+        "http://31.42.184.140/main/501000/b41ce081c95a5c4864bec8488a7a6387/%28Political%20Economy%20of%20Institutions%20and%20Decisions%29%20Elinor%20Ostrom%20-%20Governing%20the%20Commons_%20The%20Evolution%20of%20Institutions%20for%20Collective%20Action%20%28Political%20Economy%20of%20Institutions%20and%20Decisions%29-Cambridge.djvu",
+        got.http
+    );
 }
 
 #[derive(Debug)]
